853c90d75c0db6ecaa263fbe517bc3880c391313
Vivekraj Singh <vivekrajsinghchouhan2425@gmail.com>
2025-08-11 11:58:06 +0530

    Add files via upload

---
+++ BankSimulation.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Account {
+    protected String accountHolder;
+    protected double balance;
+    protected List<String> transactionHistory;
+
+    public Account(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+        this.transactionHistory = new ArrayList<>();
+        transactionHistory.add("Account created with balance: " + initialBalance);
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            transactionHistory.add("Deposited: " + amount);
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            transactionHistory.add("Withdrew: " + amount);
+            System.out.println("Withdrawal successful. New balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+        }
+    }
+
+    public void printTransactionHistory() {
+        System.out.println("Transaction History for " + accountHolder + ":");
+        for (String transaction : transactionHistory) {
+            System.out.println(transaction);
+        }
+    }
+}
+
+// SavingsAccount subclass
+class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(String accountHolder, double initialBalance, double interestRate) {
+        super(accountHolder, initialBalance);
+        this.interestRate = interestRate;
+    }
+
+    // Overriding withdraw method
+    @Override
+    public void withdraw(double amount) {
+        if (balance - amount >= 500) { // Minimum balance rule
+            super.withdraw(amount);
+        } else {
+            System.out.println("Cannot withdraw. Minimum balance of 500 required.");
+        }
+    }
+
+    public void addInterest() {
+        double interest = balance * interestRate / 100;
+        deposit(interest);
+        transactionHistory.add("Interest added: " + interest);
+    }
+}
+
+public class BankSimulation {
+    public static void main(String[] args) {
+        SavingsAccount acc1 = new SavingsAccount("Vivekraj Singh", 2000, 5);
+
+        acc1.deposit(1000);
+        acc1.withdraw(300);
+        acc1.addInterest();
+        acc1.withdraw(2500); // Should fail due to minimum balance rule
+        acc1.printTransactionHistory();
+    }
+}
